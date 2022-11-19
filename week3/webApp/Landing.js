3768dda3d463f85ce7558e5dbae5fd91c731f20c
krenee411 <kelci.morgan@bryanuniversity.edu>
2022-11-18 20:39:01 -0600

    webApp added

---
+++ week3/webApp/Landing.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Landing() {
+  return (
+    // HOME/LANDING PAGE
+    <div>
+        <h1>Welcome To Pensacoola</h1>
+        <h2>Its your first class tricket to the COOLER side of Pensacola</h2>
+       
+       {/* COOL beach picture? */}
+
+        <p>Follow us down the path less traveled</p>
+        <p>Places full of family fun and great adventures</p>
+
+        {/* memory carousel pictures */}
+
+</div>
+  )
+}
+
+export default Landing
